5968b3ec7c7e05badbb65c1204d195d52f3b63c2
Artur Gawron <artur@gawron.me>
2020-02-17 18:42:01 +0100

    Add check is empty function

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const search = document.getElementById('search'),
 
 function searchMeal(event) {
     event.preventDefault()
+
+    const term = search.value;
+
+    console.log(term);
+
+    //CHECK IS EMPTY
+    if (term.trim()) {
+
+    } else {
+        alert('Please enter a search term!');
+    }
 }
 
 //EVENT LISTENERS
